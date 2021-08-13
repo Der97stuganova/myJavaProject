309a67cdfa005ab75412cba626c46925f0af2e4e
Der97stuganova <86957187+Der97stuganova@users.noreply.github.com>
2021-08-13 15:24:44 +0300

    Delete java.Lessons/src/main/java/learn/andersen/school/lessons/lesson4 directory

--- java.Lessons/src/main/java/learn/andersen/school/lessons/lesson4/task8_interface/ShapeAction.java
+++
@@ -1,8 +0,0 @@
-package learn.andersen.school.lessons.lesson4.task8_interface;
-
-public class AbstractShape {
-    private long shapeId;
-    public long getShapeId() {
-        return shapeId;
-    }
-}
