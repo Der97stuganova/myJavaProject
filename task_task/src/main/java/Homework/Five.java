a7d40dcec5e5967a12cb0ce76d72fa56899b9e26
Der97stuganova <aderstuganova@gmail.com>
2022-01-14 14:46:21 +0300

    new commit

---
+++ task_task/src/main/java/Homework/Five.java
@@ -0,0 +1,15 @@
+package Homework;
+/*Проверить простое ли число?
+(число называется простым, если оно делится только само на себя и на 1)
+ */
+public class Five<x> {
+    public static void main(String[] args) {
+    int number;
+    int x = 3;
+        number = x;
+    if (((x%x)==0) && ((x%1)==0))
+        System.out.println("prostoe chislo");
+    else
+        System.out.println("neprostoe chislo");
+    }
+}
